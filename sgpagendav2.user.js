ac5dcdd946302ce28168c9ddfa5ad91c5cbc3b0e
Roberth <157840340+roberth1silva@users.noreply.github.com>
2025-06-06 10:30:03 -0300

    Update sgpagendav2.user.js

--- sgpagendav2.user.js
+++ sgpagendav2.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SGP Agenda Plus & Design v2
 // @namespace    http://tampermonkey.net/
-// @version      2.3
+// @version      2.4
 // @description  SGP Agenda Plus & Design v2
 // @author       Roberth
 // @match        https://fastconnect.sgp.net.br/admin/atendimento/agenda/view/*
