2e4994bbec35eef8d9bc17f6c4fdac6354341616
Roberth <157840340+roberth1silva@users.noreply.github.com>
2025-08-25 11:42:49 -0300

    Update sgptextarea.user.js

--- sgptextarea.user.js
+++ sgptextarea.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         SGP Textarea
 // @namespace    http://tampermonkey.net/
-// @version      2024-10-21
+// @version      2025-08-25
 // @description  SGP Textarea
 // @author       Roberth
 // @match        https://fastconnect.sgp.net.br/admin/atendimento/ocorrencia/os/*
+// @match        https://fastfibra.sgp.net.br/admin/atendimento/ocorrencia/os/*
 // @match        http://45.164.128.5:8000/admin/atendimento/ocorrencia/os/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @require      https://code.jquery.com/jquery-3.7.1.min.js
@@ -16,3 +17,4 @@ $('#id_conteudo').height(innerHeight_conteudo);
 
 var innerHeight_servicoprestado = $('#id_servicoprestado').prop('scrollHeight');
 $('#id_servicoprestado').height(innerHeight_servicoprestado);
+
